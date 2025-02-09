43f2fbfc51c330dc05f4f0d8c3a8ac1c107b3699
abhishek <abhishekgharami1998@gmail.com>
2025-02-09 18:38:46 +0530

    setting up the app and the connecting with the project .

--- menusite/articles/models.py
+++ menusite/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# article model
+class Article(models.Model):
+    # two field right now
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    
+    
+    def __str__(self):
+        return self.title
+    
