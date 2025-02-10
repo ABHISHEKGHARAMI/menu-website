e0324956b51d63209847c78712e3bea771a5e4b5
abhishek <abhishekgharami1998@gmail.com>
2025-02-10 21:30:31 +0530

    Adding the sample data for the user.

--- menusite/articles/views.py
+++ menusite/articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+# first view for the list of all article
+def list_article(request):
+    articles = Article.objects.all()
+    return render(
+        request,
+        'list.html',
+        {
+            'articles' : articles
+        }
+    )
