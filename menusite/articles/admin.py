10904e420d613945efb1a47005856b2f0b2b5557
abhishek <abhishekgharami1998@gmail.com>
2025-02-11 18:55:42 +0530

    Registering the model .

--- menusite/articles/admin.py
+++ menusite/articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_filter = ['title']
+    search_fields = ['title']
