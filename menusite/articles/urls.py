bf55219231a16fb27b47dfc802a94708316eb2af
abhishek <abhishekgharami1998@gmail.com>
2025-02-11 19:57:24 +0530

    checking the search bar.

--- menusite/articles/urls.py
+++ menusite/articles/urls.py
@@ -7,6 +7,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('',views.list_article,name='list_article'),
+    path('article/search/', views.article_search_view, name='search'),
     path('articles/detail/<int:pk>/',views.detail_view,name='detail'),
 ]
 
--- menusite/articles/views.py
+++ menusite/articles/views.py
@@ -25,3 +25,22 @@ def detail_view(request,pk):
             'article':article
         }
     )
+    
+    
+#  views for the search bar for the user
+def article_search_view(request):
+    article_object = None
+    context = {}
+    if request.GET:
+        query = request.GET('q')
+        article_object = Article.objects.get(id=query)
+        if article_object is not None:
+            context = {
+                'object': article_object
+            }
+    
+    return render(
+        request,
+        'search.html',
+        context=context
+    )
