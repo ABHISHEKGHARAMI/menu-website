3c1b8431ddd70c6072a98b21ec6af612ea6321d2
abhishek <abhishekgharami1998@gmail.com>
2025-02-11 11:36:32 +0530

    change the url for for correct state .

--- menusite/articles/urls.py
+++ menusite/articles/urls.py
@@ -7,6 +7,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('',views.list_article,name='list_article'),
-    path('detail/<int:pk>/',views.detail_view,name='article_detail'),
+    path('articles/detail/<int:pk>/',views.detail_view,name='detail'),
 ]
 
