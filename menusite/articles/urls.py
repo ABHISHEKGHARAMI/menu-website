de8d5b8d23bd225f519da0a4fcb29bc8d9268c46
abhishek <abhishekgharami1998@gmail.com>
2025-02-11 10:56:04 +0530

    changing the view.

--- menusite/articles/urls.py
+++ menusite/articles/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
 
+
+# app name should be there i guess
+app_name = 'articles'
+
 urlpatterns = [
     path('',views.list_article,name='list_article'),
     path('detail/<int:pk>/',views.detail_view,name='article_detail'),
