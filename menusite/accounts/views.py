067003cf4107725bf39d4f3fcef918ba7c785fbb
abhishek <abhishekgharami1998@gmail.com>
2025-02-12 20:56:09 +0530

    setting up the accounts app.

--- menusite/accounts/views.py
+++ menusite/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#  user login view
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        return  render(request,
+                       'accounts/login.html',
+                       {})
