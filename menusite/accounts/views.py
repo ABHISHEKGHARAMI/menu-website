727d35dbc7c0004f6b10e7c598b72e0d1cd977a5
abhishek <abhishekgharami1998@gmail.com>
2025-02-12 21:23:21 +0530

    logging in the user for the session .

--- menusite/accounts/views.py
+++ menusite/accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 
@@ -9,6 +10,19 @@ def login_view(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        return  render(request,
+        # print(username,password)
+        user = authenticate(request,username=username,password=password)
+        
+        if user is None:
+            context = {'error' : 'Invalid authentication'}
+            return render(request,'accounts/login.html',context=context)
+        
+        #  login the user
+        login(request,user)
+        return redirect('articles:list_article')
+        
+    return  render(request,
                        'accounts/login.html',
-                       {})
+                       {
+                        
+                       })
