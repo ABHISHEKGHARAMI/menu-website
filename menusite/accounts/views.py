cd605af2c657272f08e633452d154de179ad7581
abhishek <abhishekgharami1998@gmail.com>
2025-02-13 13:16:47 +0530

    checking working or not .

--- menusite/accounts/views.py
+++ menusite/accounts/views.py
@@ -34,6 +34,7 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
+        return redirect('accounts:login')
         
     return render(request,
                   'accounts/logout.html',{})
