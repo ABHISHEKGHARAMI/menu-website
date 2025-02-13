3776bc3f7d83c4a6da9d3290add3c8e3fdce7217
abhishek <abhishekgharami1998@gmail.com>
2025-02-13 14:11:31 +0530

    raising the validation error for the user .

--- menusite/articles/forms.py
+++ menusite/articles/forms.py
@@ -4,12 +4,14 @@ from django import forms
 #  creating the form for creating the user to write the article
 class ArticleForm(forms.Form):
     title = forms.CharField()
-    content = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
     
     
     # clean the data for the form
     def clean_title(self):
         cleaned_data = self.cleaned_data
         title = cleaned_data.get('title').lower()
+        if title is None:
+            raise forms.ValidationError('title is required for article to submit.')
         print(title)
         return title
