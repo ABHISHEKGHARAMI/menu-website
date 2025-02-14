c020de3c49301fff66fc100a8984c7c4dedc1650
abhishek <abhishekgharami1998@gmail.com>
2025-02-14 11:54:27 +0530

    changing the env for the security.

--- menusite/menusite/settings.py
+++ menusite/menusite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-np-qtx9)x3f9gw-5^l3-is#f5rkpgp6#1a30(uef6hym^c62qh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
 
 # Application definition
