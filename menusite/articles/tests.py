a77e74ddadf7b3b9dbfc781dfcb91621e3902bd7
abhishek <abhishekgharami1998@gmail.com>
2025-02-16 21:24:42 +0530

    first test case for  article for project .

--- menusite/articles/tests.py
+++ menusite/articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
+from django.conf import settings
 # Create your tests here.
+
+
+class ArticleTest(TestCase):
+    
+    # test case for the secret key strength
+    def test_password_strength(self):
+        password = settings.SECRET_KEY
+        self.assertNotEqual(password,'abc1234@')
