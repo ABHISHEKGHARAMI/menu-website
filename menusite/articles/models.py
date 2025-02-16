75b00719257af18fee7704dc4acc4d2b66076e91
abhishek <abhishekgharami1998@gmail.com>
2025-02-16 22:03:09 +0530

    changing the model for change .

--- menusite/articles/models.py
+++ menusite/articles/models.py
@@ -7,6 +7,8 @@ class Article(models.Model):
     # two field right now
     title = models.CharField(max_length=250)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     
     def __str__(self):
