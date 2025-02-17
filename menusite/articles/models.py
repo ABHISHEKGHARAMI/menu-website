c1958aae05e2ebadf240705f4d0791b9ac59d348
abhishek <abhishekgharami1998@gmail.com>
2025-02-17 15:43:15 +0530

    Adding the migrations.

--- menusite/articles/models.py
+++ menusite/articles/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+    publish = models.DateField(auto_now_add=False,auto_now=False,blank=True,null=True)
     
     def __str__(self):
         return self.title
