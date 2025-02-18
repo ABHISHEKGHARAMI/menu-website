11354f0ce6c0790b5cdd1f54d50f796c61360805
abhishek <abhishekgharami1998@gmail.com>
2025-02-18 13:26:56 +0530

    second test.

--- menusite/articles/tests.py
+++ menusite/articles/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from django.conf import settings
 from django.contrib.auth.password_validation import validate_password
+from .models import Article
 # Create your tests here.
 
 
@@ -15,6 +16,11 @@ class ArticleTest(TestCase):
         except e:
             msg = f"Bad Secret key : {e}"
             self.fail(e)
+            
+    #  setting up the test database
+    def setUp(self):
+        Article.objects.create(title='test Article',
+                               content='testing data!!')
             
             
     #  test case for the database data exist or not
