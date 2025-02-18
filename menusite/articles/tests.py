796ceecd04e576ab0f6165c99cb5023a0e71182f
abhishek <abhishekgharami1998@gmail.com>
2025-02-18 22:01:40 +0530

    testing for the slug.

--- menusite/articles/tests.py
+++ menusite/articles/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.conf import settings
 from django.contrib.auth.password_validation import validate_password
 from .models import Article
+from django.utils.text import slugify
 # Create your tests here.
 
 
@@ -32,4 +33,12 @@ class ArticleTest(TestCase):
     def test_queryset_count(self):
         qs = Article.objects.all()
         self.assertEqual(qs.count(),self.number_of_article)
-         
+        
+        
+    # test case for the slug
+    def test_slug_qs(self):
+        obj = Article.objects.all().order_by('id').first()
+        title = obj.title
+        slug = obj.slug
+        slugified_title = slugify(title)
+        self.assertEqual(slug,slugified_title)
