58676ff3fb50f09c630518cc779b7ce6ef4cdc44
abhishek <abhishekgharami1998@gmail.com>
2025-02-19 11:24:55 +0530

    this is get absolute url for th article .

--- menusite/articles/models.py
+++ menusite/articles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_save,post_save
 from .utils import slugify_instance_title
+from django.urls import reverse
 
 # Create your models here.
 
@@ -13,6 +14,12 @@ class Article(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(auto_now_add=False,auto_now=False,null=True,blank=True)
+    
+    
+    # creating the absolute view for the article
+    def get_absolute_url(self):
+        return reverse("articles:detail", kwargs={"slug":self.slug})
+    
     
     
     # save the slug method
