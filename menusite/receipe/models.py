04b714f81ac5b54e68caaeb062dd1af065ab6b3d
abhishek <abhishekgharami1998@gmail.com>
2025-02-20 13:12:22 +0530

    Model for the receipi site .

--- menusite/receipe/models.py
+++ menusite/receipe/models.py
@@ -1,4 +1,28 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
+# receipi website for the model
+class UserReceipi(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    # like grilled chicken pasta
+    description = models.TextField(null=True,blank=True)
+    directions = models.TextField(null=True,blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated =  models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    
+# ingredients for receipi 
+class UserReceipiIngredient(models.Model):
+    receipi = models.ForeignKey(Receipi,on_delete=True)
+    name = models.CharField(max_length=220)
+    description = models.TextField(null=True,blank=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    direction = models.TextField(null=True,blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
