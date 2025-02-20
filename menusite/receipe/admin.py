3b4b3c95fa19676bf6c27afce55fedff384a8a2c
abhishek <abhishekgharami1998@gmail.com>
2025-02-20 13:53:01 +0530

    Added the models to the admin panel .

--- menusite/receipe/admin.py
+++ menusite/receipe/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Receipi, ReceipiIngredient
 # Register your models here.
+
+@admin.register(Receipi)
+class ReceipiAdmin(admin.ModelAdmin):
+    list_display = ['user','name','description','directions','timestamp','updated','active']
+    list_filter = ['name','updated']
+    search_fields = ['name','timestamp','updated']
+    
+    
+@admin.register(ReceipiAdmin)
+class ReceipiIngredientAdmin(admin.ModelAdmin):
+    list_display = ['receipi','name','description','quantity','unit','direction','timestamp','updated','active']
+    list_filter = ['name','updated']
+    search_fields = ['name','timestamp','active']
