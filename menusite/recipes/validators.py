2c9458782876f8464ea3d2c15543cab741ef2f6c
abhishek <abhishekgharami1998@gmail.com>
2025-03-24 20:02:36 +0530

    working on unit for the recipe .

--- menusite/recipes/validators.py
+++ menusite/recipes/validators.py
@@ -1,11 +1,11 @@
 # this is the unit validators for checking later we update for third party package
 from django.core.exceptions import ValidationError
 
-valid_unit_measurements = ['kg','pound','ounce']
+valid_unit_measurements = ['kg','pound','ounce','oz','lbs']
 
 
 def validate_unit_of_measurement(value):
-    if value in validate_unit_of_measurement:
+    if value not in valid_unit_measurements:
         raise ValidationError(f'{value} is not a valid unit of measure..')
     
 
