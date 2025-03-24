6594c58a0b13d1dcc6b8d48cbea24a1e4e0999e4
abhishek <abhishekgharami1998@gmail.com>
2025-03-24 16:54:14 +0530

    writing the recipe test for the main brunch .

--- menusite/recipes/tests.py
+++ menusite/recipes/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Recipe, RecipeIngredient
 
 # Create your tests here.
 
@@ -24,8 +25,27 @@ class RecipeTestCase(TestCase):
     # set up function
     def setUp(self):
         self.user_a = User.objects.create_user(username='Abhi',password='Suraj1998@')
+        # creating a model recipe for the user
+        self.recipe_a = Recipe.objects.create(
+            user = self.user_a,
+            name = 'Grilled chicken'
+        )
         
     # test user count
     def test_user_count(self):
         qs = User.objects.all()
         self.assertEqual(qs.count(),1)
+        
+    # test user recipe count
+    def test_user_recipe_reverse_count(self):
+        user = self.user_a
+        qs = user.recipe_set.all()
+        self.assertEqual(qs.count(),1)
+        
+    # test user forward recipe count
+    def test_user_recipe_forward_count(self):
+        user = self.user_a
+        qs = Recipe.objects.filter(user=user)
+        self.assertTrue(qs.count(),1)
+        
+    
