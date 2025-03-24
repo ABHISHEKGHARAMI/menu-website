315b1d6fc98aa25213401151a11a83bb1aa1b880
abhishek <abhishekgharami1998@gmail.com>
2025-03-24 21:08:24 +0530

    Adding the over riding of different method .

--- menusite/recipes/models.py
+++ menusite/recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from .validators import validate_unit_of_measurement
+from .utils import number_str_to_float
 
 # Create your models here.
 
@@ -25,4 +26,13 @@ class RecipeIngredient(models.Model):
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    # over-riding the save method for the float quantity
+    def save(self,*args,**kwargs):
+        qty = self.quantity
+        qty_as_float , qty_as_float_success = number_str_to_float(qty)
+        if qty_as_float_success:
+            self.quantity_as_float = qty_as_float
+        super().save(*args,**kwargs)
+    
     
