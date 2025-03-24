f2be71590994be7a546789f445f47726b1089fcf
abhishek <abhishekgharami1998@gmail.com>
2025-03-24 20:11:17 +0530

    done with the unit for good now have to write the test .

--- menusite/recipes/validators.py
+++ menusite/recipes/validators.py
@@ -1,12 +1,23 @@
 # this is the unit validators for checking later we update for third party package
 from django.core.exceptions import ValidationError
+import pint
+from pint.errors import UndefinedUnitError
 
 valid_unit_measurements = ['kg','pound','ounce','oz','lbs']
 
 
 def validate_unit_of_measurement(value):
-    if value not in valid_unit_measurements:
-        raise ValidationError(f'{value} is not a valid unit of measure..')
+    # registry to work with the pint
+    ureg = pint.UnitRegistry()
+    
+    # if value not in valid_unit_measurements:
+    #     raise ValidationError(f'{value} is not a valid unit of measure..')
+    try:
+        single_unit = ureg[value]
+    except UndefinedUnitError as e:
+        raise ValidationError(f'{e}')
+    except ValidationError as e:
+        raise ValidationError(f'{e}')
     
 
 
