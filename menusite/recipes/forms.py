44dcbd59311623318eb9ff5cf555edf2bf9182a2
abhishek <abhishekgharami1998@gmail.com>
2025-03-27 18:12:07 +0530

    Creating the form for instant update or create.

--- menusite/recipes/forms.py
+++ menusite/recipes/forms.py
@@ -1,6 +1,6 @@
 # creating the new recipe form for the user
 from django import forms
-from .models import Recipe
+from .models import Recipe , RecipeIngredient
 
 
 # creating the form
@@ -8,3 +8,10 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name','description','directions']
+        
+        
+# creating the form for the ingredients
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ['name','quantity','unit']
