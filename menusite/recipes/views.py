badef9ab1e2d774715893a62e53900e1bb668dd2
abhishek <abhishekgharami1998@gmail.com>
2025-03-27 18:52:16 +0530

    Update partially working .

--- menusite/recipes/views.py
+++ menusite/recipes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , get_object_or_404 , redirect
 from django.contrib.auth.decorators import login_required
+from django.forms import modelformset_factory
 from .forms import RecipeForm , RecipeIngredientForm
-from .models import Recipe
+from .models import Recipe , RecipeIngredient
 
 # Create your views here.
 
@@ -58,17 +59,28 @@ def recipe_create_view(request):
 def recipe_update_view(request,id=None):
     obj = get_object_or_404(Recipe,id=id,user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
-    form_2 = RecipeIngredientForm(request.POST or None)
+    # form_2 = RecipeIngredientForm(request.POST or None)
+    # FormSet = modelformset_factory()
+    RecipeIngredientFormSet = modelformset_factory(RecipeIngredient,form=RecipeIngredientForm,extra=0)
+    qs = obj.recipeingredient_set.all()
+    formset = RecipeIngredientFormSet(request.POST or None, queryset=qs)
     context = {
         'form' : form,
-        "form_2" : form_2,
+        'formset' : formset,
         'obj'  : obj
     }
     # check form is valid or not
-    if all([form.is_valid(),form_2.is_valid()]):
+    if all([form.is_valid(),formset.is_valid()]):
         # if true then save in an object
-        form.save(commit=False)
-        form_2.save(commit=False)
+        # this is two different form but they are connected through a foreign key
+        # so have to save this way
+        parent = form.save(commit=False)
+        parent.save()
+        for form in formset:
+            child = form.save(commit=False)
+            if child.recipe is None:
+                child.recipe = parent
+            child.save()
         context['message'] = 'data saved...'
     return render(
         request,
